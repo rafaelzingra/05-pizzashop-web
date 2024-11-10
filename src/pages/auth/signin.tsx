5dbd600ba639e0068990dc3a4ce5929d370da9f1
RafaelZingra <rafaelzingra@hotmail.com>
2024-11-10 18:39:25 -0300

    adds toast feature

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,6 +4,7 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { toast } from "sonner"
 
 export function Signin() {
 
@@ -15,8 +16,13 @@ export function Signin() {
     type SigninForm = z.infer<typeof signinForm>
     
     async function handleSignin(data: SigninForm) {
-        console.log(data)
-        await new Promise(resolve => setTimeout(resolve,2000))
+        try {
+            console.log(data)            
+            await new Promise(resolve => setTimeout(resolve,2000))
+            toast.success('Enviamos um link de autenticação para seu email!')
+        } catch {
+            toast.error('Email inválido!')
+        }
     }
 
     return (
