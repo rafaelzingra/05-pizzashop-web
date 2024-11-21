d6ac0330596b87089c95950c9b05db5a4e84e633
RafaelZingra <rafaelzingra@hotmail.com>
2024-11-20 21:41:44 -0300

    adds dashboard cards

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/dashboard";
+import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { Signin } from "./pages/auth/signin";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
