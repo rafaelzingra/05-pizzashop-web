465ba94483b746366e1744128ea09880c141913a
RafaelZingra <rafaelzingra@hotmail.com>
2024-11-20 21:58:59 -0300

    adds 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,13 @@ import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Signup } from "./pages/auth/signup";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter ([
     {
         path: '/',
         element: <AppLayout />,
+        errorElement: <NotFound />,
         children: [
             {path: '/', element: <Dashboard />},
             {path: '/orders', element: <Orders />}
