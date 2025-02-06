a4c52fe1607f3b145d37c2bcbeb38f7341e8d1e6
Rafael Zingra <rafaelzingra@hotmail.com>
2025-02-05 21:30:06 -0300

    adds 404 pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,33 +1,46 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/dashboard/dashboard";
-import { Signin } from "./pages/auth/signin";
-import { AppLayout } from "./pages/_layouts/app";
-import { AuthLayout } from "./pages/_layouts/auth";
-import { Signup } from "./pages/auth/signup";
-import { Orders } from "./pages/app/orders/orders";
-import { NotFound } from "./pages/404";
+import { createBrowserRouter } from 'react-router-dom'
 
-export const router = createBrowserRouter ([
-    {
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Orders } from './pages/app/orders/orders'
+import { Signin } from './pages/auth/signin'
+import { Signup } from './pages/auth/signup'
+import { Error } from './pages/error'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
         path: '/',
-        element: <AppLayout />,
-        // errorElement: <NotFound />,
-        children: [
-            {path: '/', element: <Dashboard />},
-            {path: '/orders', element: <Orders />}
-        ]
-    },
-    
-    {
-        path: '/',
-        element: <AuthLayout />,
-        children: [
-            {path: '/signin', element: <Signin />},
-            {path: '/signup', element: <Signup />}
-        ],        
-    },
-    {
-        path: '*',
-        element: <NotFound />
-    }
+        element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <Signin />,
+      },
+      {
+        path: '/sign-up',
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
 ])
