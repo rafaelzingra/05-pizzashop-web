e58807f654a5fc34f8d27a8f76a825b4e8bf451d
Rafael Zingra <rafaelzingra@hotmail.com>
2025-02-09 19:52:30 -0300

    adds mocked data to dashboard

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,8 +1,25 @@
 import { setupWorker } from 'msw/browser'
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount'
+import { getMonthCancelledOrdersAmountMock } from './get-month-cancelled-orders-amount'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount'
+import { getMonthRevenueMock } from './get-month-revenue'
+import { getPopularProductsMock } from './get-popular-products-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
 
 import { env } from '@/env'
+import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker()
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCancelledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
